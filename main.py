72e3a9046337c60ae39b7cf2682f1c3759961667
bernard <bernardfounder@gmail.com>
2019-08-06 17:34:40 +0700

    Change message post to slack.

--- main.py
+++ main.py
@@ -5,33 +5,28 @@ import logging
 import boto3
 import jinja2
 import json
+import slack
+import io
+
+from base64 import b64decode
+
 
 # setup logger
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+#logger = logging.getLogger()
+# logger.setLevel(logging.INFO)
 
-# Get SNS ARN from environment variable
-snsTargetArn = os.environ['SNS_TARGET_ARN']
+#slackChannel = os.environ["SLACK_CHANNEL"]
+#slackToken = os.environ["SLACK_TOKEN"]
 
 
-def publish_to_sns(message, subject):
-    client = boto3.client("sns")
-    try:
-        response = client.publish(
-            TargetArn=snsTargetArn,
-            Message=message,
-            Subject=subject,
-            MessageStructure="string"
-        )
-    except Exception as error:
-        print(error)
+sc = slack.WebClient(token=slackToken)
 
 
 def lambda_handler(event, context):
     """
     main lambda function for handling events of AWS infrastructure health notification
     """
-    logger.info('Event: ' + str(event))
+    #logger.info('Event: ' + str(event))
 
     service = event["detail"]["service"]
 
@@ -46,6 +41,20 @@ def lambda_handler(event, context):
         eventDescription=event_description,
         resources=affected_resources
     )
-    subject = "[Amazon " + service + " Maintenance] " + \
-        event['detail']['eventTypeCode']
-    publish_to_sns(message, subject)
+    try:
+        response = sc.files_upload(
+            channels=slackChannel,
+            content=message,
+            filetype="post",
+            filename="Amazon " + service + " Scheduled Maintenance",
+            title="Amazon " + service + " Scheduled Maintenance"
+        )
+        print(response)
+
+    except Exception as error:
+        print(error)
+
+if __name__ == "__main__":
+    with open("event_example.json") as json_file:
+        data = json.load(json_file)
+        lambda_handler(data, "context")
